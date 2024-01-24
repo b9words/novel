691e852fa3f7dd68508891fac9a24f47e5b21b88
Jake Kim <b9words@gmail.com>
2024-01-24 10:11:15 +0900

    scoped preflight

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,8 +1,13 @@
+const { scopedPreflightStyles } = require("tailwindcss-scoped-preflight");
 const sharedConfig = require("tailwind-config/tailwind.config.js");
 
 module.exports = {
   presets: [sharedConfig],
-  corePlugins: {
-    preflight: false,
-  }
+  plugins: [
+    // ... other plugins
+    scopedPreflightStyles({
+      cssSelector: ".twp", // or .tailwind-preflight or even [data-twp=true] - any valid CSS selector of your choice
+      mode: "matched only", // it's the default
+    }),
+  ],
 };
